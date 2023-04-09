05d922cd0c9a8b820d644cfda0b7af03e551f5df
Finlay Hlannon <79950262+finlayhlannon@users.noreply.github.com>
2023-04-08 21:17:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
-from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='.')
 
-jots = []
+import random
+import string
+from flask import Flask, request, render_template, jsonify, redirect
+
+app = Flask(__name__, template_folder=".")
+
+# Define a dictionary to store the jots
+jots = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Define a function to generate a unique URL
+# Define a function to generate a unique URL
+def generate_url():
+    chars = string.ascii_lowercase + string.digits
+    url = ''.join(random.choice(chars) for _ in range(6))
+    while url in jots:
+        url = ''.join(random.choice(chars) for _ in range(6))
+    return url
+
+
+# Define the route to create a jot
 @app.route('/create_jot', methods=['POST'])
 def create_jot():
     name = request.form['name']
-    content = request.form['jotspace']
-    jot = {'name': name, 'content': content}
-    jots.insert(0, jot)
-    return render_template('stream.html', jots=jots)
+    jotspace = request.form['jotspace']
+    url = generate_url()
+    jots[url] = {'name': name, 'jotspace': jotspace}
+    print(url)  # Debugging line
+    return redirect(f'/jot/{url}')
 
 
-@app.route('/stream')
-def stream():
-    return render_template('stream.html', jots=jots)
 
+# Define the route to view a jot
+@app.route('/jot/<url>')
+def view_jot(url):
+    jot = jots.get(url)
+    if jot:
+        return render_template('jot.html', jot=jot)
+    else:
+        return 'Jot not found'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
