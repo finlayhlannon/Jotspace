e10775c3483b93c18d525267693f54425be1dbaf
Finlay Hlannon <79950262+finlayhlannon@users.noreply.github.com>
2023-04-12 21:23:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -57,13 +57,13 @@ def index():
     global wview_timer
     current_time = int(time.time())
     if current_time - view_timer >= 86400:  # 86400 seconds in 24 hours
-        view_count = 998
+        view_count = 0
         view_timer = current_time
     if current_time - wview_timer >= 604800:  # 604800 seconds in 7 days
-        wview_count = 997
+        wview_count = 0
         wview_timer = current_time
     if current_time - mview_timer >= 2592000:  # 2592000 seconds in 30 days
-        mview_count = 996
+        mview_count = 0
         mview_timer = current_time
 
     view_count += 1
